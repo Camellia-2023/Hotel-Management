4cb6490a2f7cb5c6df5eeb3a93f5c898c347e57e
zi041010 <154664647+zi041010@users.noreply.github.com>
2024-06-26 15:47:42 +0800

    客房管理模块的实现
    主要定义了Gust Room Manager类

---
+++ room_manager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "guest_room.h" 
+#include<map> 
+#include<string> 
+class GuestRoomManager      //定义客房管理类 
+{
+public:
+	GuestRoomManager();      //构造函数 
+	bool check_in(const string);    //查找客房 
+	void add_room(const GuestRoom room);   //添加客房 
+	bool remove_data(const string);    //删除客房数据 
+	void set_room_state(string number);  //设置客房状态 
+private:
+	map<string, GuestRoom> m_room_list;  //map容器 
+};
